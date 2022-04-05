fcc421b6b8224b9251ffcb0886931c4e8f641443
chrischen86 <chrischen86@gmail.com>
2022-04-05 02:51:42 -0400

    Replace text buttons with icons

--- src/components/SiteAppBar.tsx
+++ src/components/SiteAppBar.tsx
@@ -5,11 +5,14 @@ import {
   Toolbar,
   Typography,
 } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import BarChartIcon from '@material-ui/icons/BarChart';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import GroupIcon from '@material-ui/icons/Group';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
-
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -27,37 +30,70 @@ const useStyles = makeStyles((theme) => ({
 
 const SiteAppBar = () => {
   const classes = useStyles();
+  const showTextButtons = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up('sm')
+  );
+
   return (
     <AppBar position="static">
       <Toolbar>
         <Typography variant="h6" className={classes.title}></Typography>
-        <Button
-          component={RouterLink}
-          to={'/'}
-          variant={'text'}
-          className={classes.button}
-          size={'large'}
-        >
-          Stats
-        </Button>
-        <Button
-          component={RouterLink}
-          to={'/autoplay'}
-          variant={'text'}
-          className={classes.button}
-          size={'large'}
-        >
-          Autoplay
-        </Button>
-        <Button
-          component={RouterLink}
-          to={'/manage'}
-          variant={'text'}
-          className={classes.button}
-          size={'large'}
-        >
-          Manage
-        </Button>
+        {!showTextButtons && (
+          <>
+            <IconButton
+              component={RouterLink}
+              to={'/'}
+              className={classes.button}
+            >
+              <BarChartIcon />
+            </IconButton>
+            <IconButton
+              component={RouterLink}
+              to={'/autoplay'}
+              className={classes.button}
+            >
+              <PlayArrowIcon />
+            </IconButton>
+            <IconButton
+              component={RouterLink}
+              to={'/manage'}
+              className={classes.button}
+            >
+              <GroupIcon />
+            </IconButton>
+          </>
+        )}
+        {showTextButtons && (
+          <>
+            <Button
+              component={RouterLink}
+              to={'/'}
+              variant={'text'}
+              className={classes.button}
+              size={'large'}
+            >
+              Stats
+            </Button>
+            <Button
+              component={RouterLink}
+              to={'/autoplay'}
+              variant={'text'}
+              className={classes.button}
+              size={'large'}
+            >
+              Autoplay
+            </Button>
+            <Button
+              component={RouterLink}
+              to={'/manage'}
+              variant={'text'}
+              className={classes.button}
+              size={'large'}
+            >
+              Manage
+            </Button>
+          </>
+        )}
         <IconButton
           color="inherit"
           target="_blank"
